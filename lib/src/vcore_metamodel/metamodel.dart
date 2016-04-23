9a944ec1873762cb46f3a6375e68a6b68e288e90
Anders Holmgren <andersmholmgren@gmail.com>
2016-04-23 13:01:38 +1000

    spiking

---
+++ lib/src/vcore_metamodel/metamodel.dart
@@ -0,0 +1,24 @@
+import 'package:vcore/src/model/model.dart';
+
+Package get vcorePackage => _vcorePackage ??= _createPackage();
+
+Package _vcorePackage;
+
+Package _createPackage() {
+
+  final packageBuilder = new PackageBuilder()..name='vcore';
+  final classifiers = packageBuilder.classifiers;
+  classifiers..add(_createValueClass())..add(_createValueClass());
+  return packageBuilder.build();
+}
+
+ValueClass _createNamedElementClass() {
+  final builder = (new ValueClassBuilder()..name = 'NamedElement');
+  builder.properties..add((new PropertyBuilder()..name='name').build());
+
+  return builder.build();
+}
+
+ValueClass _createValueClass() {
+
+}
