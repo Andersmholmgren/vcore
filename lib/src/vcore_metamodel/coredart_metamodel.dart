5fd69b6fed469934a18a59dc046615eee12dfb5d
Anders Holmgren <andersmholmgren@gmail.com>
2016-04-23 13:46:37 +1000

    spiking

---
+++ lib/src/vcore_metamodel/coredart_metamodel.dart
@@ -0,0 +1,21 @@
+import 'package:vcore/src/model/model.dart';
+
+Package get dartPackage => _dartPackage ??= _createPackage();
+
+Package _dartPackage;
+
+Package _createPackage() {
+  final packageBuilder = new PackageBuilder()..name = 'dart';
+  final classifiers = packageBuilder.classifiers;
+  classifiers..add(dartString);
+  return packageBuilder.build();
+}
+
+ExternalClass _dartString;
+ExternalClass get dartString => _dartString ??= _createDartString();
+
+ExternalClass _createDartString() {
+  final builder = new ExternalClassBuilder()..name = 'String';
+
+  return builder.build();
+}
--- lib/src/vcore_metamodel/metamodel.dart
+++ lib/src/vcore_metamodel/metamodel.dart
@@ -1,5 +1,7 @@
 import 'package:vcore/src/model/model.dart';
 
+import 'coredart_metamodel.dart';
+
 Package get vcorePackage => _vcorePackage ??= _createPackage();
 
 Package _vcorePackage;
@@ -7,32 +9,36 @@ Package _vcorePackage;
 Package _createPackage() {
   final packageBuilder = new PackageBuilder()..name = 'vcore';
   final classifiers = packageBuilder.classifiers;
-  classifiers..add(_namedElement)..add(_valueClass);
+  classifiers..add(namedElement)..add(valueClass);
   return packageBuilder.build();
 }
 
-ValueClass __namedElement;
-ValueClass get _namedElement => __namedElement ??= _createNamedElement();
+ValueClass _namedElement;
+ValueClass get namedElement => _namedElement ??= _createNamedElement();
 
 ValueClass _createNamedElement() {
   final builder = new ValueClassBuilder()..name = 'NamedElement';
-  builder.properties..add((new PropertyBuilder()..name = 'name').build());
+  builder.properties
+    ..add((new PropertyBuilder()
+          ..name = 'name'
+          ..type = dartString)
+        .build());
 
   return builder.build();
 }
 
-ValueClass __classifier;
-ValueClass get _classifier => __classifier ??= _createClassifier();
+ValueClass _classifier;
+ValueClass get classifier => _classifier ??= _createClassifier();
 
 ValueClass _createClassifier() {
   final builder = new ValueClassBuilder()..name = 'Classifier';
-  builder.superTypes.add(_namedElement);
+  builder.superTypes.add(namedElement);
 
   return builder.build();
 }
 
-ValueClass __valueClass;
-ValueClass get _valueClass => __valueClass ??= _createValueClass();
+ValueClass _valueClass;
+ValueClass get valueClass => _valueClass ??= _createValueClass();
 
 ValueClass _createValueClass() {
   final builder = new ValueClassBuilder()..name = 'ValueClass';
