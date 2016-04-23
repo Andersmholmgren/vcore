d3d0fd8ce13f4ac970796127f1248f3c70d74946
Anders Holmgren <andersmholmgren@gmail.com>
2016-04-23 13:20:53 +1000

    spiking

--- lib/src/vcore_metamodel/metamodel.dart
+++ lib/src/vcore_metamodel/metamodel.dart
@@ -5,20 +5,37 @@ Package get vcorePackage => _vcorePackage ??= _createPackage();
 Package _vcorePackage;
 
 Package _createPackage() {
-
-  final packageBuilder = new PackageBuilder()..name='vcore';
+  final packageBuilder = new PackageBuilder()..name = 'vcore';
   final classifiers = packageBuilder.classifiers;
-  classifiers..add(_createNamedElementClass())..add(_createValueClass());
+  classifiers..add(_namedElement)..add(_valueClass);
   return packageBuilder.build();
 }
 
-ValueClass _createNamedElementClass() {
-  final builder = (new ValueClassBuilder()..name = 'NamedElement');
-  builder.properties..add((new PropertyBuilder()..name='name').build());
+ValueClass __namedElement;
+ValueClass get _namedElement => __namedElement ??= _createNamedElement();
+
+ValueClass _createNamedElement() {
+  final builder = new ValueClassBuilder()..name = 'NamedElement';
+  builder.properties..add((new PropertyBuilder()..name = 'name').build());
 
   return builder.build();
 }
 
-ValueClass _createValueClass() {
+ValueClass __classifier;
+ValueClass get _classifier => __classifier ??= _createClassifier();
 
+ValueClass _createClassifier() {
+  final builder = new ValueClassBuilder()..name = 'Classifier';
+  builder.superTypes.add(_namedElement);
+
+  return builder.build();
+}
+
+ValueClass __valueClass;
+ValueClass get _valueClass => __valueClass ??= _createValueClass();
+
+ValueClass _createValueClass() {
+  final builder = new ValueClassBuilder()..name = 'ValueClass';
+  builder.superTypes.add(_classifier);
+//  builder.properties.add()
 }
