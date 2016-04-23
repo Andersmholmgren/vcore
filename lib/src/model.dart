ff20d572fef4f3c8097dd99ddd64f5e03dcbe1b3
Anders Holmgren <andersmholmgren@gmail.com>
2016-04-23 10:35:57 +1000

    spiking

--- lib/src/model.dart
+++ lib/src/model.dart
@@ -1,7 +1,7 @@
 library vcore.model;
 
-import 'package:built_collection/built_collection.dart';
-import 'package:built_json/built_json.dart';
+//import 'package:built_collection/built_collection.dart';
+//import 'package:built_json/built_json.dart';
 import 'package:built_value/built_value.dart';
 
 part 'model.g.dart';
@@ -9,45 +9,29 @@ part 'model.g.dart';
 // Copyright (c) 2016, <your name>. All rights reserved. Use of this source code
 // is governed by a BSD-style license that can be found in the LICENSE file.
 
-abstract class NamedElement
-    implements Built<NamedElement, NamedElementBuilder> {
+abstract class NamedElement {
   String get name;
-  NamedElement._();
-
-  factory NamedElement([updates(NamedElementBuilder b)]) = _$NamedElement;
 }
 
-abstract class NamedElementBuilder
-    implements Builder<NamedElement, NamedElementBuilder> {
+abstract class NamedElementBuilder {
   String name;
-  NamedElementBuilder._();
-  factory NamedElementBuilder() = _$NamedElementBuilder;
 }
 
-abstract class Classifier //extends NamedElement
-    implements Built<Classifier, ClassifierBuilder> {
-  Classifier._();
+abstract class Classifier implements NamedElement {}
 
-  factory Classifier([updates(ClassifierBuilder b)]) = _$Classifier;
-
-}
-
-abstract class ClassifierBuilder
-  implements Builder<Classifier, ClassifierBuilder> {
-  ClassifierBuilder._();
-  factory ClassifierBuilder() = _$ClassifierBuilder;
-}
-
+abstract class ClassifierBuilder implements NamedElementBuilder {}
 
 abstract class ValueClass extends Classifier
     implements Built<ValueClass, ValueClassBuilder> {
+  String get name;
   ValueClass._();
 
   factory ValueClass([updates(ValueClassBuilder b)]) = _$ValueClass;
 }
 
-abstract class ValueClassBuilder
+abstract class ValueClassBuilder /*extends NamedElementBuilder*/
     implements Builder<ValueClass, ValueClassBuilder> {
+  String name;
   ValueClassBuilder._();
   factory ValueClassBuilder() = _$ValueClassBuilder;
 }
--- lib/src/model.g.dart
+++ lib/src/model.g.dart
@@ -1,26 +1,25 @@
 // GENERATED CODE - DO NOT MODIFY BY HAND
-// 2016-04-22T23:50:42.201180Z
+// 2016-04-23T00:30:39.283772Z
 
 part of vcore.model;
 
 // **************************************************************************
 // Generator: BuiltValueGenerator
-// Target: abstract class NamedElement
+// Target: abstract class ValueClass
 // **************************************************************************
 
-class _$NamedElement extends NamedElement {
+class _$ValueClass extends ValueClass {
   final String name;
-  _$NamedElement._({this.name}) : super._() {
+  _$ValueClass._({this.name}) : super._() {
     if (name == null) throw new ArgumentError('null name');
   }
-  factory _$NamedElement([updates(NamedElementBuilder b)]) =>
-      (new NamedElementBuilder()..update(updates)).build();
-  NamedElement rebuild(updates(NamedElementBuilder b)) =>
+  factory _$ValueClass([updates(ValueClassBuilder b)]) =>
+      (new ValueClassBuilder()..update(updates)).build();
+  ValueClass rebuild(updates(ValueClassBuilder b)) =>
       (toBuilder()..update(updates)).build();
-  _$NamedElementBuilder toBuilder() =>
-      new _$NamedElementBuilder()..replace(this);
+  _$ValueClassBuilder toBuilder() => new _$ValueClassBuilder()..replace(this);
   bool operator ==(other) {
-    if (other is! NamedElement) return false;
+    if (other is! ValueClass) return false;
     return name == other.name;
   }
 
@@ -29,100 +28,24 @@ class _$NamedElement extends NamedElement {
   }
 
   String toString() {
-    return 'NamedElement {'
+    return 'ValueClass {'
         'name=${name.toString()}\n'
         '}';
   }
 }
 
-class _$NamedElementBuilder extends NamedElementBuilder {
-  _$NamedElementBuilder() : super._();
-  void replace(NamedElement other) {
+class _$ValueClassBuilder extends ValueClassBuilder {
+  _$ValueClassBuilder() : super._();
+  void replace(ValueClass other) {
     super.name = other.name;
   }
 
-  void update(updates(NamedElementBuilder b)) {
-    if (updates != null) updates(this);
-  }
-
-  NamedElement build() {
-    if (name == null) throw new ArgumentError('null name');
-    return new _$NamedElement._(name: name);
-  }
-}
-
-// **************************************************************************
-// Generator: BuiltValueGenerator
-// Target: abstract class Classifier
-// **************************************************************************
-
-class _$Classifier extends Classifier {
-  _$Classifier._() : super._() {}
-  factory _$Classifier([updates(ClassifierBuilder b)]) =>
-      (new ClassifierBuilder()..update(updates)).build();
-  Classifier rebuild(updates(ClassifierBuilder b)) =>
-      (toBuilder()..update(updates)).build();
-  _$ClassifierBuilder toBuilder() => new _$ClassifierBuilder()..replace(this);
-  bool operator ==(other) {
-    if (other is! Classifier) return false;
-    return true;
-  }
-
-  int get hashCode {
-    return 922550110;
-  }
-
-  String toString() {
-    return 'Classifier {}';
-  }
-}
-
-class _$ClassifierBuilder extends ClassifierBuilder {
-  _$ClassifierBuilder() : super._();
-  void replace(Classifier other) {}
-  void update(updates(ClassifierBuilder b)) {
-    if (updates != null) updates(this);
-  }
-
-  Classifier build() {
-    return new _$Classifier._();
-  }
-}
-
-// **************************************************************************
-// Generator: BuiltValueGenerator
-// Target: abstract class ValueClass
-// **************************************************************************
-
-class _$ValueClass extends ValueClass {
-  _$ValueClass._() : super._() {}
-  factory _$ValueClass([updates(ValueClassBuilder b)]) =>
-      (new ValueClassBuilder()..update(updates)).build();
-  ValueClass rebuild(updates(ValueClassBuilder b)) =>
-      (toBuilder()..update(updates)).build();
-  _$ValueClassBuilder toBuilder() => new _$ValueClassBuilder()..replace(this);
-  bool operator ==(other) {
-    if (other is! ValueClass) return false;
-    return true;
-  }
-
-  int get hashCode {
-    return 498947498;
-  }
-
-  String toString() {
-    return 'ValueClass {}';
-  }
-}
-
-class _$ValueClassBuilder extends ValueClassBuilder {
-  _$ValueClassBuilder() : super._();
-  void replace(ValueClass other) {}
   void update(updates(ValueClassBuilder b)) {
     if (updates != null) updates(this);
   }
 
   ValueClass build() {
-    return new _$ValueClass._();
+    if (name == null) throw new ArgumentError('null name');
+    return new _$ValueClass._(name: name);
   }
 }
