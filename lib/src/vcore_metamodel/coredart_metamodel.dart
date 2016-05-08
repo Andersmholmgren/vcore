05574d5f78ae95589165e27c213b574c6d391102
Anders Holmgren <andersmholmgren@gmail.com>
2016-05-08 11:35:56 +1000

    using vcore in jason schemer

--- lib/src/vcore_metamodel/coredart_metamodel.dart
+++ lib/src/vcore_metamodel/coredart_metamodel.dart
@@ -7,7 +7,13 @@ Package _dartPackage;
 Package _createPackage() {
   final packageBuilder = new PackageBuilder()..name = 'dart';
   final classifiers = packageBuilder.classifiers;
-  classifiers..add(dartObject)..add(dartString)..add(dartBool);
+  classifiers
+    ..add(dartObject)
+    ..add(dartString)
+    ..add(dartBool)
+    ..add(dartNum)
+    ..add(dartInt)
+    ..add(dartNull);
   return packageBuilder.build();
 }
 
@@ -22,5 +28,17 @@ ExternalClass _createDartString() => _ec('String');
 ExternalClass _dartBool;
 ExternalClass get dartBool => _dartBool ??= _createDartBool();
 ExternalClass _createDartBool() => _ec('bool');
+
+ExternalClass _dartNum;
+ExternalClass get dartNum => _dartNum ??= _createDartNum();
+ExternalClass _createDartNum() => _ec('num');
+
+ExternalClass _dartInt;
+ExternalClass get dartInt => _dartInt ??= _createDartInt();
+ExternalClass _createDartInt() => _ec('int');
+
+ExternalClass _dartNull;
+ExternalClass get dartNull => _dartNull ??= _createDartNull();
+ExternalClass _createDartNull() => _ec('null');
 
 ExternalClass _ec(String name) => new ExternalClass((b) => b..name = name);
